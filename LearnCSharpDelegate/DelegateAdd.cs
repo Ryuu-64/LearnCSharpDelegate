cdc9427c6d0d6042698c7103d7874c9cfd478e82
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2022-11-26 16:25:51 +0800

    initial commit

---
+++ LearnCSharpDelegate/DelegateAdd.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+namespace LearnCSharpDelegate
+{
+    internal static class DelegateAdd
+    {
+        private static int res = 0;
+
+        private static void ResIncrease()
+        {
+            res++;
+        }
+
+        [SetUp]
+        public static void SetUp()
+        {
+            res = 0;
+        }
+
+        [Test]
+        public static void MulticastDelegateAddTest()
+        {
+            Action action1 = ResIncrease;
+            Action action2 = () => { };
+            action2 += action1;
+            action1 += ResIncrease;
+            action2.Invoke();
+            AreEqual(1, res);
+        }
+    }
+}

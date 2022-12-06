bd11c224a1464a5dbe2e2ad00fd8bf47723f507e
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2022-12-06 13:32:03 +0800

    clean up code in MulticastDelegateTest

--- LearnCSharpDelegate/MulticastDelegateTest.cs
+++ LearnCSharpDelegate/MulticastDelegateTest.cs
@@ -3,14 +3,9 @@ using static NUnit.Framework.Assert;
 
 namespace LearnCSharpDelegate
 {
-    internal static class DelegateAdd
+    internal static class MulticastDelegateTest
     {
-        private static int res = 0;
-
-        private static void ResIncrease()
-        {
-            res++;
-        }
+        private static int res;
 
         [SetUp]
         public static void SetUp()
@@ -19,14 +14,19 @@ namespace LearnCSharpDelegate
         }
 
         [Test]
-        public static void MulticastDelegateAddTest()
+        public static void MulticastDelegateSideEffectTest()
         {
             Action action1 = ResIncrease;
             Action action2 = () => { };
             action2 += action1;
-            action1 += ResIncrease;
+            action1 += ResIncrease; // no effect to action2
             action2.Invoke();
             AreEqual(1, res);
+        }
+
+        private static void ResIncrease()
+        {
+            res++;
         }
     }
 }

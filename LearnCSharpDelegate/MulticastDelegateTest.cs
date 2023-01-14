13a1ed684baeaf62cad1cc7e81222c57e71e9571
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2023-01-14 15:55:41 +0800

    MulticastDelegateTest

--- LearnCSharpDelegate/MulticastDelegateTest.cs
+++ LearnCSharpDelegate/MulticastDelegateTest.cs
@@ -1,32 +1,69 @@
 ﻿using NUnit.Framework;
 using static NUnit.Framework.Assert;
 
-namespace LearnCSharpDelegate
+namespace LearnCSharpDelegate;
+
+internal static class DelegateAdd
 {
-    internal static class MulticastDelegateTest
+    [Test]
+    public static void SideEffectAddTest()
     {
-        private static int res;
+        int res = 0;
+        Action action1 = () => { };
+        Action action2 = () => res++;
+        action1 += action2;
+        action2 += () => res++;
+        action1.Invoke();
+        AreEqual(1, res);
+    }
 
-        [SetUp]
-        public static void SetUp()
+    [Test]
+    public static void SideEffectRemoveTest()
+    {
+        int res = 0;
+        Action action1 = () => { };
+        Action action2 = () => res++;
+        action1 += action2;
+        action1 += () => action1 -= action2;
+        action1.Invoke();
+        AreEqual(1, res);
+    }
+
+    [Test]
+    public static void EqualsTest()
+    {
+        Action action1 = DummyMethod;
+        Action action2 = DummyMethod;
+        Action action3 = () => { };
+
+        action1 += action3;
+        action2 += action3;
+
+        True(action1.Equals(action2));
+        True(action2.Equals(action1));
+
+        void DummyMethod()
         {
-            res = 0;
         }
+    }
 
-        [Test]
-        public static void MulticastDelegateSideEffectTest()
-        {
-            Action action1 = ResIncrease;
-            Action action2 = () => { };
-            action2 += action1;
-            action1 += ResIncrease; // no effect to action2
-            action2.Invoke();
-            AreEqual(1, res);
-        }
+    [Test]
+    public static void GetHashCodeTest()
+    {
+        Action action1 = DummyMethod;
+        Action action2 = DummyMethod;
+        Action action3 = () => { };
 
-        private static void ResIncrease()
+        action1 += action3;
+        action2 += action3;
+
+        int hashCode1 = action1.GetHashCode();
+        int hashCode2 = action2.GetHashCode();
+
+        AreEqual(hashCode1, hashCode2);
+
+        void DummyMethod()
         {
-            res++;
         }
     }
 }
